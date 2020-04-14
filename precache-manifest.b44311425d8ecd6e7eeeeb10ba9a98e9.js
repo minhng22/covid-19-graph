bf34141ff643c522ef15b1e9db9526cfb12475df
MinhNguyen <adsbox@icloud.com>
2020-04-13 20:49:30 -0500

    Updates

---
+++ precache-manifest.b44311425d8ecd6e7eeeeb10ba9a98e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed32b1493212abb24a4ff7a3e2a582b2",
+    "url": "/covid-19-graph/index.html"
+  },
+  {
+    "revision": "2e1cee491985704771e3",
+    "url": "/covid-19-graph/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ec5ea833d2b81db2ea1e",
+    "url": "/covid-19-graph/static/js/2.650235ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-graph/static/js/2.650235ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e1cee491985704771e3",
+    "url": "/covid-19-graph/static/js/main.3487e40d.chunk.js"
+  },
+  {
+    "revision": "6d9216d22ee0d1cdc0cb",
+    "url": "/covid-19-graph/static/js/runtime-main.02f0c3fd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-19-graph/static/media/logo.5d5d9eef.svg"
+  }
+]);
